9f624f9893e01bef6c1230e3734c45adf6d8c2ca
Vaheloid <vlad.tyupin@yandex.ru>
2025-12-18 18:01:28 +0500

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+import uvicorn
+
+app = FastAPI()
+
 
 data = {
-    "email": "name@mail",
-    "bio": None,
-    "age": 12
+    "email": "name@mail.ru",
+    "bio": "Биография",
+    "age": 12,
 }
+
+data_wo_age = {
+    "email": "name@mail.ru",
+    "bio": "Биография",
+    #"gender": "male",
+    #"birthday": "2000"
+}
+
+class UserSchema(BaseModel):
+    email: EmailStr
+    bio: str | None = Field(max_length=10)
+    
+    model_config = ConfigDict(extra='forbid')
+
+users = []
+
+@app.post("/users")
+def add_user(user: UserSchema):
+    users.append(user)
+    return {"ok": True, "message": "User добавлен"}
+
+@app.get("/users")
+def get_users() -> list[UserSchema]:
+    return users
+
+#class UserAgeSchema(UserSchema):
+#    age: int = Field(ge=0, le=130)
+
+#print(repr(UserSchema(**data_wo_age)))
+#print(repr(UserAgeSchema(**data)))
